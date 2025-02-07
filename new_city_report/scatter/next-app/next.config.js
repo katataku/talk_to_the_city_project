e5b311118f5394f472c1901fc3030d8c1150377a
Junwei Wu <junweiwu.me@gmail.com>
2025-02-07 23:47:24 +0900

    new config

--- new_city_report/scatter/next-app/next.config.js
+++ new_city_report/scatter/next-app/next.config.js
@@ -6,20 +6,12 @@ const nextConfig = !report
   ? {
       output: "export",
       distDir: "out",
-      server: {
-        host: '0.0.0.0',
-        port: 3000
-      }
     }
   : {
       output: "export",
       distDir: `../pipeline/outputs/${report}/report`,
       assetPrefix: "./",
       env: { REPORT: report },
-      server: {
-        host: '0.0.0.0',
-        port: 3000
-      }
     };
 
 module.exports = nextConfig;
